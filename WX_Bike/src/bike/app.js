f95b655134e5a482dba319c2b3eadcdd111fd491
LaZY <18846171630@163.com>
2019-03-26 17:26:20 +0800

    add:微信小程序:记录用户唯一id（openid）——code

--- WX_Bike/src/bike/app.js
+++ WX_Bike/src/bike/app.js
@@ -1,34 +1,28 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
+    // 登录,然后获取到用户的唯一身份ID，用于以后记log
     wx.login({
       success: res => {
+        //根据你的微信小程序的密钥到后台获取ID
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+        if (res.code) {
+          var appid = "wx5b603fddb2c1bca9";
+          var secret = "ca6b9becf4ff51692f01fa86065ad35c";
+          var code = res.code;
+          //发起网络请求
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
+            success: function (r) {
+              //获取到每个用户的对立id
+              //console.log(r.data.openid)
+              //把openid保存到本地
+              wx.setStorageSync('openid', r.data.openid)
             }
           })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
         }
       }
     })
