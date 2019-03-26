fbf50e91d7be060b0430dd7629e0f2bc6b6254ba
LaZY <18846171630@163.com>
2019-03-26 22:10:02 +0800

    add:使用spark分析数据（pv、uv）——code

---
+++ BikeSpark/src/main/scala/MongoSparkSQL.scala
@@ -0,0 +1,43 @@
+package cn.edu360.day2
+
+import com.mongodb.spark.MongoSpark
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+/**
+  * Created by zx on 2017/10/8.
+  * https://docs.mongodb.com/spark-connector/current/scala/datasets-and-sql/
+  */
+object MongoSparkSQL {
+
+  def main(args: Array[String]): Unit = {
+
+    val session = SparkSession.builder()
+      .master("local")
+      .appName("MongoSparkConnectorIntro")
+      .config("spark.mongodb.input.uri", "mongodb://lazy:root@192.168.255.201:27017/lazy_bike.logs")
+      .config("spark.mongodb.output.uri", "mongodb://lazy:root@192.168.255.201:27017/lazy_bike.resluts")
+      .getOrCreate()
+
+    val df: DataFrame = MongoSpark.load(session)
+
+    df.createTempView("v_logs")
+
+    //val result:DataFrame = session.sql("SELECT age, name FROM v_student WHERE age >= 30 ORDER BY age DESC")
+
+    //val result = session.sql("SELECT age, name FROM v_student WHERE age is null")
+
+    val pv = session.sql("select count(*) from v_logs")
+
+    val uv = session.sql("select count(*) pv, count(distinct openid) uv from v_logs")
+
+    //pv.show()
+
+    //uv.show()
+
+    MongoSpark.save(uv)
+    //MongoSpark
+
+    session.stop()
+
+  }
+}
