957510b4cb177659ec38cf8ae790d98633f9af85
LaZY <18846171630@163.com>
2019-03-27 16:46:11 +0800

    add:测试nginx（返回主机）——code

--- src/main/java/com/lazy/bike/controller/UserController.java
+++ src/main/java/com/lazy/bike/controller/UserController.java
@@ -2,6 +2,10 @@ package com.lazy.bike.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Controller
 public class UserController {
@@ -13,5 +17,17 @@ public class UserController {
         //前缀 + 视图名 + 后缀
         //pages/index.html
         return "index";
+    }
+    //请求映射，跟浏览器中的URL请求资源对应就会调用对应的发放
+    @RequestMapping("/host")
+    @ResponseBody
+    public String host() {
+        String host = null;
+        try {
+            host = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return host;
     }
 }
